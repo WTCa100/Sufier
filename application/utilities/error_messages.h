ebdf0f9e154bc3ad5fb6cbe512bf6fbbedd17113
WTCa100 <janbielawa00@gmail.com>
2023-07-16 19:06:26 +0200

    Added more structure and error codes
    From now on every error message / error reasone message is segregated into
    it's own respective category

--- application/utilities/error_messages.h
+++ application/utilities/error_messages.h
@@ -2,26 +2,41 @@
 #define APP_UTILITIES_ERROR_MESSAGES
 
 #include <stdio.h>
+#include <string.h>
 
 #include "logger.h"
 
 /**
- * @brief Displays error message in standarized LOG format
+ * @brief Displays simple error messages in standarized LOG format
  * @param _MSG_ Error message
  * @param _FAIL_REASON_ Cause of error
  * @return void
  */
 #define ERR_MSG(_MSG_, _FAIL_REASON_) \
 { \
-    LOG("%s%s", _MSG_, _FAIL_REASON_); \
+    LOG("%s %s", _MSG_, _FAIL_REASON_); \
 }
 
-// Errors
-#define ERR_COULD_NOT_CREATE_NODE "Error. Could not create node."
-#define ERR_COULD_NOT_ADD_CONTACT "Error. Could not add contact."
+//========//
+// Errors //
+//========//
+// Node
+#define ERR_NODE_COULD_NOT_CREATE_NODE "Error. Could not create node."
+#define ERR_NODE_COULD_NOT_ADD_CONTACT "Error. Could not add contact."
+#define ERR_NODE_COULD_NOT_DELETE_NODE "Error. Could not delete node."
+// Contact
+#define ERR_CONTACT_COULD_NOT_DISPLAY_DATA "Error. Could not display contact."
+#define ERR_CONTACT_COULD_NOT_BE_COMPARED "Error. Could not compare contacts."
 
-// Reasons
+//=========//
+// Reasons //
+//=========//
+// Common
 #define ERR_REASON_MEM_ALLOC_FAIL "Memory allocation failed."
+// Node
+#define ERR_REASON_NODE_FULL "Linked list has reached it's maximum capacity."
+#define ERR_REASON_NODE_HEAD_NULL "Node head is NULL."
+// Contact
 #define ERR_REASON_CONTACT_NULL "Contact cannot be NULL."
 
 #endif // APP_UTILITIES_ERROR_MESSAGES
